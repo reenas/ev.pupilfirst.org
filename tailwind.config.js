5a7f6873c7603a30f086f2e8e57ecd48b2f49476
vinu tv <vinutv91@gmail.com>
2021-03-19 02:53:45 +0530

    Update tailwind config with brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,34 +12,44 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        spacemono:
-          '"Space Mono", Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-        montserrat: 'Montserrat, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";'
+        inter: 'Inter, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";'
       },
       colors: {
         primary: {
-          50: '#F1FFF7',
-          100: '#CDFEE1',
-          200: '#ABFECE',
-          300: '#8EFCBC',
-          400: '#75F8AC',
-          500: '#5DF59C',
-          600: '#5BEC97',
-          700: '#48E086',
-          800: '#27D36D',
-          900: '#1FC061',
+          50:  '#eaf8f6',
+          100: '#cef7ee',
+          200: '#9cf3d9',
+          300: '#5beabf',
+          400: '#5afba6',
+          500: '#07c86d',
+          600: '#07b154',
+          700: '#0d934a',
+          800: '#107341',
+          900: '#105e38',
+        },
+        secondary: {
+          50:  '#f0f9fb',
+          100: '#d7f7f8',
+          200: '#a8eff0',
+          300: '#6de3e9',
+          400: '#13cef8',
+          500: '#0daecf',
+          600: '#0b8eb6',
+          700: '#107292',
+          800: '#11586c',
+          900: '#104655',
         },
         gray: {
-          50: '#DADADD',
-          100: '#C7C8CC',
-          200: '#ABADB4',
-          300: '#90939C',
-          400: '#6C6F79',
-          500: '#595B63',
-          600: '#494950',
-          700: '#3A3B41',
-          800: '#28292D',
-          900: '#232326',
+          50: '#F6F7F8',
+          100: '#EEEFF2',
+          200: '#DADDE2',
+          300: '#C0C4CE',
+          400: '#A7ACB9',
+          500: '#7C8498',
+          600: '#5E6678',
+          700: '#474D5C',
+          800: '#343A46',
+          900: '#21252E',
         }
       }
     },
